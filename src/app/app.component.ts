3f08a02d80ad5f69659fd597f098f1665f4b5e78
Sahil <sahil.singh98@hotmail.com>
2024-02-06 13:00:55 +0100

    Prepare Angular Parcel & Setup Build Deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-parcel-comments',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
